82f708487c02a4d7e0c417b3091807b30702c278
Karen Gonzalez <kren1504@hotmail.com>
2021-03-15 22:42:20 -0400

    solucionado

--- register_new_user.py
+++ register_new_user.py
@@ -1,59 +1,60 @@
 import unittest
-from pyunitreport import HTMLTestRunner
 from selenium import webdriver
 
 class RegisterNewUser(unittest.TestCase):
 
-    def setUp(self):
-        self.driver=webdriver.Chrome(executable_path=r'C:\Users\kren1\Documents\curso\Python_con_Selenium\chromedriver.exe')
-        driver.implicitly_wait(10)
-        driver.maximize_window()
-        driver.get('http://demo-store.seleniumacademy.com/')
+	def setUp(self):
+		self.driver = webdriver.Chrome(executable_path=r'C:\Users\kren1\Documents\curso\Python_con_Selenium\chromedriver.exe')
+		driver = self.driver
+		driver.implicitly_wait(30)
+		driver.maximize_window()
+		driver.get("http://demo.onestepcheckout.com/")
+	
+	def test_new_user(self):
+		driver = self.driver
+		#le decimos al driver que encuentre la opción de cuenta por su Xpath y le haga click para desplegar el menu
+		driver.find_element_by_xpath('/html/body/div/div[2]/header/div/div[2]/div/a/span[2]').click()
+		#el driver va a buscar el enlace por su texto y haga click
+		driver.find_element_by_link_text('Log In').click()
+		#creo una variable asociada al botón de crear cuenta
+		create_account_button = driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div/div[2]/form/div/div[1]/div[2]/a/span/span')
+		
+		#validamos que el botón esté visible y habilitado
+		self.assertTrue(create_account_button.is_displayed() and create_account_button.is_enabled())
+		create_account_button.click()
 
-    def test_new_user(self):
-        driver=self.driver
-        driver.find_elements_by_xpath('//*[@id="header"]/div/div[2]/div/a/span[2]').click()
-        driver.find_element_by_link_text("Log In")
-        
-        create_account_button=driver.find_elements_by_xpath('//*[@id="login-form"]/div/div[1]/div[2]/a/span/span')
-        self.assertTrue(create_account_button.is_displayed() and create_account_button.is_enabled())
-        create_account_button.click()
+		#comprueba que estamos en el sitio de crear cuenta
+		self.assertEqual('Create New Customer Account', driver.title)
 
-        self.assertEqual('Create New Customer Account',driver.title )
+		#creación de variables con el nombre del selector correspondiente
+		first_name = driver.find_element_by_id('firstname')
+		last_name = driver.find_element_by_id('lastname')
+		email_address = driver.find_element_by_id('email_address')
+		password = driver.find_element_by_id('password')
+		confirm_password = driver.find_element_by_id('confirmation')
+		news_letter_subscription = driver.find_element_by_id('is_subscribed')
+		submit_button = driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div/div[2]/form/div[2]/button')
 
-        first_name = driver.find_element_by_id('firstname')
-        middle_name = driver.find_element_by_id('middlename')
-        last_name = driver.find_element_by_id('lastname')
-        email_address = driver.find_element_by_id('email_address')
-        news_letter_subscription = driver.find_element_by_id('is_subscribed')
-        password = driver.find_element_by_id('password')
-        confirm_password = driver.find_element_by_id('confirmation')
-        submit_button = driver.find_elements_by_xpath('//*[@id="form-validate"]/div[2]/button/span/span')
+		#veremos si los elementos están habilitados
+		self.assertTrue(first_name.is_enabled() 
+		and last_name.is_enabled()
+		and email_address.is_enabled()
+		and password.is_enabled()
+		and confirm_password.is_enabled()
+		and news_letter_subscription.is_enabled()
+		and submit_button.is_enabled())
 
-        self.assertTrue(first_name.is_enabled()
-        and last_name.is_enabled()
-        and middle_name.is_enabled()
-        and email_address.is_enabled()
-        and news_letter_subscription.is_enabled() 
-        and password.is_enabled()
-        and confirm_password.is_enabled()
-        and submit_button.is_enabled()
-        )
+		#mandamos los datos al formulario
+		first_name.send_keys('Test')
+		last_name.send_keys('Test')
+		email_address.send_keys('arqcftlothxuknlxkt@awdrt.com') #sacado de 10-minute mail
+		password.send_keys('Test')
+		confirm_password.send_keys('Test')
+		submit_button.click()
 
-        first_name.send_keys('Test')
-        middle_name.send_keys('Test')
-        last_name.send_keys('Test')
-        email_address.send_keys('Test')
-        password.send_keys('Test')
-        confirm_password.send_keys('Test')
-        submit_button.click()
+	def tearDown(self):
+		self.driver.implicitly_wait(3)
+		self.driver.close()
 
-
-
-
-
-    def tearDown(self):
-        self.driver.quit()
-
-if __name__=="__main__":
-    unittest.main(verbosity=2)  
+if __name__ == "__main__":
+	unittest.main(verbosity = 2)
